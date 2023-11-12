d0ee18aa7ccf44d6e1f6b3b4bf0adb9744d9f099
Callum Messiter <callum.messiter@gmail.com>
2023-11-12 19:04:30 +0000

    Add poc for matrix-activity visualisation

---
+++ viz/src/App.tsx
@@ -0,0 +1,10 @@
+import { Heatmap } from './Heatmap';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Heatmap />
+      <div id="heatmap"></div>
+    </div>
+  );
+}
