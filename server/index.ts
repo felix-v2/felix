bae793d1b541b273be3274d40f683a4b84a313f4
Callum Messiter <callum.messiter@gmail.com>
2023-11-14 19:20:53 +0000

    Emit the simulated network activity periodically

--- server/index.ts
+++ server/index.ts
@@ -6,12 +6,31 @@ const { app } = expressWs(express());
 dotenv.config();
 const port = process.env.PORT;
 
+// generates a 2d matrix of X x Y neurons, each with a random activity value between 0 and 1
+const randActivity = (
+  opts: {
+    neuronsX?: number;
+    neuronsY?: number;
+    silent?: boolean;
+  } = { neuronsX: 25, neuronsY: 25 }
+) => {
+  return Array.from(Array(opts.neuronsX)).map(() =>
+    Array.from(Array(opts.neuronsY)).map(() =>
+      opts.silent ? 0 : Math.random()
+    )
+  );
+};
+
 app.get("/", (req: Request, res: Response) => {
-  app.emit("message", { hello: "world" });
   res.send("Hello, World!");
+
+  // every 600ms, emit an event containing the simulated network activity
+  setInterval(() => {
+    app.emit("new-activity", { area1: randActivity() });
+  }, 600);
 });
 
-app.on("message", (msg) => {
+app.on("new-activity", (msg) => {
   console.log(msg);
 });
 
