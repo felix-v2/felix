f8cb3bcb5ad8689892404e02cb26e76dd95335c0
Callum Messiter <callum.messiter@gmail.com>
2024-01-05 15:52:37 +0000

    Python-ify the activity generator func

--- neural-net/server/server.py
+++ neural-net/server/server.py
@@ -6,22 +6,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
+# get between 1 and 100 randomly sampled neurons from the network of 625
+# activate each neuron in the sample with a random activity level between 0 and 1
+
 
 def randActivity(neuronsX=25, neuronsY=25):
-    # get between 1 and 100 randomly sampled neurons from the network of 625
-    totalNeurons = neuronsX * neuronsY
-    activatedNeurons = random.sample(
-        range(neuronsX * neuronsY), random.randint(1, 100))
-
-    # activate each neuron in the sample with a random activity level between 0 and 1
-    neuralActivity = []
-    for i in range(totalNeurons):
-        if i in activatedNeurons:
-            neuralActivity.append(random.uniform(0, 1))
-        else:
-            neuralActivity.append(0)
-
-    return neuralActivity
+    neurons = range(neuronsX * neuronsY)
+    neuronSample = random.sample(neurons, random.randint(1, 100))
+    return [0 if i not in neuronSample else random.uniform(0, 1) for i in neurons]
 
 
 @app.route('/')
