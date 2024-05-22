fec2eec237e59e170a8333c019d510901b040aeb
Callum Messiter <callum.messiter@gmail.com>
2024-05-22 18:19:00 +0100

    Add "no learning" test case

--- server/models/__tests__/test_compute_learning.py
+++ server/models/__tests__/test_compute_learning.py
@@ -9,22 +9,49 @@ class TestComputeLearning(unittest.TestCase):
         net.main_init()
         net.init()
         net.MAIN_INIT_RANDOM_ACTIVITY()
+        net.slrate = 0.1  # learning only runs if slrate > 0
+
+        # the func modifies the np slice in place, so use copy() so we can compare before and after
+        start_rates = net.rates.copy()
+        start_pot = net.pot.copy()
+        start_j = net.J.copy()
+        start_tot_ltd = net.tot_LTD.copy()
+        start_tot_ltp = net.tot_LTP.copy()
+
+        net.compute_learning(0.1)
+
+        # each of the 6 (area) values in tot_LTP and tot_LTD have changed
+        self.assertTrue(np.all(net.tot_LTP != start_tot_ltp))
+        self.assertTrue(np.all(net.tot_LTD != start_tot_ltd))
+
+        # J updated
+        self.assertFalse(np.allclose(net.J, start_j))
+
+        # rates and pot unaffected
+        self.assertTrue(np.allclose(net.rates, start_rates))
+        self.assertTrue(np.allclose(net.pot, start_pot))
+
+    def test_no_learning(self):
+        net = StandardNet6Areas()
+        net.main_init()
+        net.INIT_RANDOM_ACTIVITY()
+
+        net.slrate = 0  # learning only runs if slrate > 0
 
         # the func modifies the np slice in place, so use copy() so we can compare before and after
         start_rates = net.rates.copy()
         start_pot = net.pot.copy()
         start_j = net.J.copy()
 
+        self.assertTrue(np.all(net.tot_LTD == 0))
+        self.assertTrue(np.all(net.tot_LTP == 0))
+
         net.compute_learning(0.1)
 
-        # each of the 6 (area) values in tot_LTP and tot_LTD updated
-        self.assertEqual(np.count_nonzero(net.tot_LTP == 0), 0)
-        self.assertEqual(np.count_nonzero(net.tot_LTD == 0), 0)
-
-        # J updated
-        self.assertFalse(np.allclose(net.J, start_j))
-
-        # rates and pot unaffected
+        # learning not computed because slrate is 0 - so nothing changes
+        self.assertTrue(np.all(net.tot_LTD == 0))
+        self.assertTrue(np.all(net.tot_LTP == 0))
+        self.assertTrue(np.allclose(net.J, start_j))
         self.assertTrue(np.allclose(net.rates, start_rates))
         self.assertTrue(np.allclose(net.pot, start_pot))
 
